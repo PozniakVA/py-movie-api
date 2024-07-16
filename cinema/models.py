1bc3c719539293e3dc69471c8a073e497d4884da
Valentin Pozniak <waliker448@gmail.com>
2024-07-16 15:06:59 +0300

    create MovieSerializers

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    duration = models.IntegerField()
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    duration = models.IntegerField(validators=[MinValueValidator(1)])
 
     class Meta:
         ordering = ["title"],
